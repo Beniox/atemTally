307a1e737334aaf5786484f385b4cfe2dae3bf74
Beniox <37512887+Beniox@users.noreply.github.com>
2023-05-30 11:31:10 +0200

    change camera with right click

--- public/script.js
+++ public/script.js
@@ -10,26 +10,29 @@ let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -
 canvas.setAttribute('height', style_height * dpi);
 canvas.setAttribute('width', style_width * dpi);
 
-const x = canvas.width / 2 -50;
-const y = canvas.height / 2+50;
+const x = canvas.width / 2 - 50;
+const y = canvas.height / 2 + 50;
 
 
 /**
  * Manage connection to server
- * @class
+ * @class Connection
  */
 class Connection {
     /**
+     * Singleton instance
      * @type {Connection}
      */
     static instance = null;
 
     /**
+     * Last received state
      * @type {Object}
      */
     lastState = null;
 
     /**
+     * Socket.IO instance
      * @type {Object}
      */
     socket = null;
@@ -40,6 +43,7 @@ class Connection {
     }
 
     /**
+     * Create a new Connection
      * @param {string | null} url
      */
     constructor(url = null) {
@@ -48,14 +52,19 @@ class Connection {
         }
         Connection.instance = this;
 
-        this.loadOptions();
-        if (url) {
+
+        this.loadOptions(); // Load options from local storage
+
+
+        if (url) { // If url is provided, use it, otherwise use default
             this.options.url = url;
         }
-        this.socket = io(this.options.url);
+
+        this.socket = io(this.options.url); // Connect to server
 
         video.srcObject = canvas.captureStream();
 
+        // Add event listeners
         this.socket.on('connect', this.connected);
         this.socket.on('disconnect', this.disconnected);
         this.socket.on('state', this.onState);
@@ -64,7 +73,6 @@ class Connection {
         this.connected = this.connected.bind(this);
         this.disconnected = this.disconnected.bind(this);
         this.onState = this.onState.bind(this);
-
     }
 
     /**
@@ -78,31 +86,40 @@ class Connection {
             return;
         }
         ErrorAlert.hide();
-        if (data.video.program === Connection.instance.options.cameraID) {
+        Connection.instance.displayData();
+
+    }
+
+    /**
+     * Display data on screen
+     */
+    displayData() {
+        // display data on the video stream
+        if (Connection.instance.lastState.video.program === Connection.instance.options.cameraID) {
             context.fillStyle = 'red';
-        } else if (data.video.preview === Connection.instance.options.cameraID) {
+        } else if (Connection.instance.lastState.video.preview === Connection.instance.options.cameraID) {
             context.fillStyle = 'green';
         } else {
             context.fillStyle = 'white';
         }
+
         context.fillRect(0, 0, canvas.width, canvas.height);
         context.fillStyle = 'black';
         context.font = '225px Arial';
         context.fillText(Connection.instance.options.cameraID, x, y);
         video.srcObject = canvas.captureStream();
 
+        // display data on the small camera divs
         for (let i = 0; i < cameras.length; i++) {
-            if (i + 1 === data.video.program) {
+            if (i + 1 === Connection.instance.lastState.video.program) {
                 cameras[i].style.backgroundColor = 'red';
-            } else if (i + 1 === data.video.preview) {
+            } else if (i + 1 === Connection.instance.lastState.video.preview) {
                 cameras[i].style.backgroundColor = 'green';
             } else {
                 cameras[i].style.backgroundColor = 'white';
             }
         }
-
     }
-
 
     /**
      * When Socket.IO throws an error
@@ -129,6 +146,9 @@ class Connection {
         Connection.instance.showError();
     }
 
+    /**
+     * When ATEM is not connected
+     */
     showError() {
         ErrorAlert.display('ATEM not connected');
         context.fillStyle = 'yellow';
@@ -184,35 +204,83 @@ class Connection {
 new Connection();
 
 
+/**
+ * Open settings page
+ */
 function openSettings() {
     document.getElementById('settingsPage').style.display = 'block';
     document.getElementById('settingsCameraID').value = Connection.instance.options.cameraID;
 }
 
+/**
+ * Close settings page
+ */
 function closeSettings() {
     document.getElementById('settingsPage').style.display = 'none';
 }
 
+/**
+ * Save settings to local storage
+ */
 function saveSettings() {
-    const cameraID = Number(document.getElementById('settingsCameraID').value);
-    Connection.instance.options.cameraID = cameraID;
+    Connection.instance.options.cameraID = Number(document.getElementById('settingsCameraID').value);
     Connection.instance.saveOptions();
+    Connection.instance.displayData();
     closeSettings();
 }
 
+/**
+ * Display error messages
+ */
 class ErrorAlert {
     static ele = document.getElementById('errorAlert');
 
+    /**
+     * Display a error message
+     * @param message {string}
+     */
     static display(message) {
         ErrorAlert.ele.innerText = message;
         ErrorAlert.ele.style.display = 'block';
     }
 
+    /**
+     * Hide the error message
+     */
     static hide() {
         ErrorAlert.ele.style.display = 'none';
     }
 }
 
+/**
+ * Starts the video, if it is not already playing
+ */
 function playVideo() {
     video.play();
 }
+
+/**
+ * Change camera
+ * @param id {number}
+ */
+function changeCamera(id) {
+    Connection.instance.options.cameraID = id;
+    Connection.instance.saveOptions();
+    Connection.instance.displayData();
+}
+
+/**
+ * Add an event listener to all cameras to change between them
+ */
+function setEventListener() {
+    const cameras = document.getElementsByClassName('cameras');
+    for (let i = 0; i < cameras.length; i++) {
+        cameras[i].addEventListener('contextmenu', function (event) {
+            changeCamera(i + 1);
+            event.preventDefault();
+        });
+        console.log(cameras[i]);
+    }
+}
+
+setEventListener();
